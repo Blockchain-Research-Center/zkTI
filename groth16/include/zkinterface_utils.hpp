6ca471d9d915a7dbeefeb431ff68927fde8834bd
LBruyne <22221263@zju.edu.cn>
2023-07-26 13:59:05 +0800

    refactor

--- groth16/include/zkinterface_utils.hpp
+++ groth16/include/zkinterface_utils.hpp
@@ -20,11 +20,9 @@ namespace zkinterface_utils {
     const mp_size_t r_limbs = alt_bn128_r_limbs;
 
     // utils
-
     void into_le(const bigint<r_limbs> &num, uint8_t *out, size_t size);
-
+    
     // reader
-
     uoffset_t read_size_prefix(void *buffer);
 
     const CircuitHeader* read_circuit_header(char *buffer);
@@ -32,27 +30,6 @@ namespace zkinterface_utils {
     const ConstraintSystem* read_constraint_system(char *buffer);
 
     const Witness* read_witness(char *buffer);
-
-    // const Command* read_command(char *buffer);
-
-    // vector<char>::iterator next_message(vector<char>::iterator it);
-
-    // // Find the first message of the requested type in a buffer.
-    // const Root *find_message(char *buffer, Message type);
-
-    // // find_message, with buffer size validation.
-    // const Root *find_message(char *buffer, uoffset_t buffer_size, Message type);
-
-    // // find_message in a vector, with buffer size validation.
-    // const Root *find_message(vector<char> &buffer, Message type);
-
-    // const KeyValue *find_config(const Circuit *circuit, string key);
-
-    // string find_config_text(const Circuit *circuit, string key, string default_ = "");
-
-    // const Vector<uint8_t> *find_config_data(const Circuit *circuit, string key);
-
-    // int64_t find_config_number(const Circuit *circuit, string key, int64_t default_ = 0);
 
     class MessageNotFoundException : public std::exception {
     public:
